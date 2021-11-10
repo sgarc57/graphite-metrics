0cf73757da1dd44555b3ed02c4b781bf27d656cb
steven garcia <sgarcia00@gmail.com>
2021-11-10 21:46:52 +0100

    exposing additional methods for metrics service

--- src/main/java/org/perpetualnetworks/graphitemetrics/MetricsService.java
+++ src/main/java/org/perpetualnetworks/graphitemetrics/MetricsService.java
@@ -1,5 +1,11 @@
 package org.perpetualnetworks.graphitemetrics;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.LongTaskTimer;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+
 public interface MetricsService {
 
     public void start();
@@ -8,4 +14,13 @@ public interface MetricsService {
 
     public void close();
 
+    public MeterRegistry getMeterRegistry();
+
+    public Timer fetchTimer(String timerName);
+
+    public Counter fetchCounter(String counterName);
+
+    public Gauge fetchGauge(String gaugename);
+
+    public LongTaskTimer fetchLongTaskTimer(String longTaskTimerName);
 }
--- src/main/java/org/perpetualnetworks/graphitemetrics/MetricsServiceImpl.java
+++ src/main/java/org/perpetualnetworks/graphitemetrics/MetricsServiceImpl.java
@@ -4,6 +4,7 @@ import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.LongTaskTimer;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.Timer;
 
@@ -20,31 +21,43 @@ public class MetricsServiceImpl implements MetricsService {
         return this.graphiteMeterRegistry;
     }
 
+    @Override
     public void start() {
         getGraphiteMeterRegistry().start();
     }
 
+    @Override
     public void close() {
         getGraphiteMeterRegistry().close();
         stop();
     }
 
+    @Override
+    public MeterRegistry getMeterRegistry() {
+        return getGraphiteMeterRegistry();
+    }
+
+    @Override
     public void stop() {
         getGraphiteMeterRegistry().stop();
     }
 
+    @Override
     public Timer fetchTimer(String timerName) {
         return getGraphiteMeterRegistry().get(timerName).timer();
     }
 
+    @Override
     public Counter fetchCounter(String counterName) {
         return getGraphiteMeterRegistry().get(counterName).counter();
     }
 
+    @Override
     public Gauge fetchGauge(String gaugename) {
         return getGraphiteMeterRegistry().get(gaugename).gauge();
     }
 
+    @Override
     public LongTaskTimer fetchLongTaskTimer(String longTaskTimerName) {
         return getGraphiteMeterRegistry().get(longTaskTimerName).longTaskTimer();
     }
